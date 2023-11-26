4d3d8ed6e96c626ac1de0684dc9e60ece1555b36
Fazliddin Khayrullaev <fazliddinkhayrullaev4@gmail.com>
2023-11-26 20:31:50 +0500

    Register

--- js/all-posts.js
+++ js/all-posts.js
@@ -8,7 +8,7 @@ const businessArticlesWrapper = document.querySelector(
 );
 const searchInput = document.querySelector(".search-input ", ".business-input");
 const newsAPI =
-  "https://newsapi.org/v2/everything?q=tesla&from=2023-10-26&sortBy=publishedAt&apiKey=934cb19591e64940909373a050f8d182";
+  "https://newsapi.org/v2/everything?q=all&from=2023-10-26&sortBy=publishedAt&apiKey=934cb19591e64940909373a050f8d182";
 
 //Navbar responsive
 toggleBtn.addEventListener("click", () => {
@@ -30,7 +30,7 @@ async function getNews(url) {
 }
 
 //Search input
-searchInput?.addEventListener("input", async () => {
+searchInput.addEventListener("input", async () => {
   const searchValue = searchInput.value.trim().toLowerCase();
   const searchedData = await getNews(
     `https://newsapi.org/v2/everything?q=${searchValue}&from=2023-10-26&sortBy=publishedAt&apiKey=934cb19591e64940909373a050f8d182`
@@ -42,7 +42,8 @@ searchInput?.addEventListener("input", async () => {
 
 //Render data
 function renderNews(newsData) {
-  // businessArticlesWrapper.innerHTML = "";
+  businessArticlesWrapper.innerHTML = "";
+
   newsData.slice(0, 4).forEach((news) => {
     if (news.name != "[Removed]" || news.content !== "[Removed]") {
       //  console.log(news?.description);
@@ -81,68 +82,5 @@ function renderNews(newsData) {
 }
 
 getNews(
-  "https://newsapi.org/v2/everything?q=tesla&from=2023-10-26&sortBy=publishedAt&apiKey=934cb19591e64940909373a050f8d182"
+  "https://newsapi.org/v2/everything?q=all&from=2023-10-26&sortBy=publishedAt&apiKey=934cb19591e64940909373a050f8d182"
 ).then((data) => renderNews(data.articles));
-
-const formRegister = document.querySelector(".register__form");
-const firstname = document.querySelector(".firstname");
-const lastname = document.querySelector(".lastname");
-const username = document.querySelector(".username");
-const password = document.querySelector(".password");
-const passwordConfirm = document.querySelector(".confirm__password");
-const usersWrapper = document.querySelector(".users");
-
-const error = document.querySelector(".error");
-const btnDelete = document.createElement('button');
-formRegister.addEventListener("submit", (e) => {
-  e.preventDefault();
-  formRegister.reset();
-  createPost();
-});
-
-
-async function createPost() {
-  
-  const firstnameValue=firstname.value.trim()
-  const lastnameValue = lastname.value.trim();
-  const usernameValue = username.value.trim();
-  const userID=new Date().getMilliseconds()
-
-
-
-  const user = {
-    firstname: firstnameValue,
-    lastname: lastnameValue,
-    username: usernameValue,
-    userID:userID
-  };
-
-   const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-     method: "POST",
-     headers: {
-       "Content-type": "application/json ;   charset=UTF-8",
-     },
-     body:JSON.stringify({user})
-   });
-
-if(response.ok){
-    const newPost=response.json();
-    console.log(newPost);
-    const userBox = document.createElement("div");
-    userBox.classList.add(".user");
-
-    const firstname=document.createElement('h2');
-    firstname.textContent=newPost.firstname
-    const lastname=document.createElement('h3');
-    lastname.textContent = newPost.lastname;
-    const username = document.createElement("h4");
-    username.textContent = newPost.username;
-   
-    usersWrapper.appendChild(userBox)
-     userBox.appendChild(firstname);
-     userBox.appendChild(lastname);
-     userBox.appendChild(username);
-     userBox.appendChild(btnDelete);
-   }
-  
-}
